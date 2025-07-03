777abfcd6ee1edafbb00cfd5a4e67d72af11f68f
danielz0102 <danyzglez@hotmail.com>
2025-07-03 17:25:45 -0600

    Validate file size on client

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -5,13 +5,22 @@ setUp()
 
 const fileBtn = document.querySelector('.file-button')
 const fileInput = document.querySelector('#file')
+const DEFAULT_FILE_NAME = 'Select a file'
 
 fileBtn.addEventListener('click', () => {
   fileInput.click()
 })
 fileInput.addEventListener('change', () => {
   const file = fileInput.files[0]
-  fileBtn.textContent = file ? file.name : 'Select a file'
+  fileBtn.textContent = file ? file.name : DEFAULT_FILE_NAME
+
+  if (file.size > 50 * 1024 * 1024) {
+    fileInput.value = ''
+    fileBtn.textContent = DEFAULT_FILE_NAME
+    fileInput.setCustomValidity('The file cannot be larger than 50MB.')
+  } else {
+    fileInput.setCustomValidity('')
+  }
 })
 
 function setUp() {
