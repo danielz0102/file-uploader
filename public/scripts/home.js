fc1a7a322b44d5a66d2eb0f449a9fe3deeea10ca
danielz0102 <danyzglez@hotmail.com>
2025-07-03 14:49:43 -0600

    Style folder modal

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,1 +1,15 @@
 import './partials/form.js'
+
+const newFolderDialogButton = document.querySelector('#newFolderDialogButton')
+const newFolderDialog = document.querySelector('#newFolderDialog')
+const closeNewFolderDialogButton = document.querySelector(
+  '#closeNewFolderDialogButton',
+)
+
+newFolderDialogButton.addEventListener('click', () => {
+  newFolderDialog.showModal()
+})
+
+closeNewFolderDialogButton.addEventListener('click', () => {
+  newFolderDialog.close()
+})
